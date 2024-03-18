4294cd915867af3578394a0aa98db0b22c57bad1
lokher <lokher@gmail.com>
2024-03-18 10:05:26 +0800

    fix delay of macro doens't work properly except cable mode

--- quantum/send_string/send_string.c
+++ quantum/send_string/send_string.c
@@ -23,6 +23,10 @@
 #include "keycode.h"
 #include "action.h"
 #include "wait.h"
+#ifdef LK_WIRELESS_ENABLE
+#include "wireless.h"
+#endif
+
 
 #if defined(AUDIO_ENABLE) && defined(SENDSTRING_BELL)
 #    include "audio.h"
@@ -179,8 +183,17 @@ void send_string_with_delay(const char *string, uint8_t interval) {
                     ms += keycode - '0';
                     keycode = *(++string);
                 }
+<<<<<<< HEAD
 
                 wait_ms(ms);
+=======
+                while (ms--) {
+#ifdef LK_WIRELESS_ENABLE
+                    send_string_task();
+#endif
+                    wait_ms(1);
+                }
+>>>>>>> 0cd4ee07c3 (fix delay of macro doens't work properly except cable mode)
             }
 
             wait_ms(interval);
@@ -189,6 +202,22 @@ void send_string_with_delay(const char *string, uint8_t interval) {
         }
 
         ++string;
+<<<<<<< HEAD
+=======
+        // interval
+        {
+            uint8_t ms = interval;
+            while (ms--) {
+#ifdef LK_WIRELESS_ENABLE
+                send_string_task();
+#endif
+                wait_ms(1);
+            }
+        }
+#ifdef LK_WIRELESS_ENABLE
+        send_string_task();
+#endif
+>>>>>>> 0cd4ee07c3 (fix delay of macro doens't work properly except cable mode)
     }
 }
 
